49cd2b09f3364ab7e8e5dda955a211b599b7f509
SimonNordon4 <simon.nordon@protonmail.com>
2023-01-08 14:49:26 +1000

    Renaming

---
+++ Assets/god-object/God.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GodObject{
+    public class God : UnityEngine.MonoBehaviour
+    {
+        public void Start()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
+

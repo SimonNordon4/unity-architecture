74e2ac14d1a4722347beb5ad454c8b4a1d613664
Simon Nordon <86404984+SimonNordon4@users.noreply.github.com>
2023-01-11 14:56:59 +1000

    Improved commenting for game controller.

--- Assets/Architectures/01_Monoscript/scripts/GameController.cs
+++ Assets/Architectures/01_Monoscript/scripts/GameController.cs
@@ -9,11 +9,25 @@ using Random = UnityEngine.Random;
 namespace MonoScript
 {
     /// <summary>
-    /// Controls all of the Scene Logic.
+    /// Controls the game logic for this scene.
+    /// Representative of how a new developer might approach game development.
+    /// This class is responsible for:
+    ///     Controlling the Camera
+    ///     Controlling the Player Input
+    ///     Spawning Enemies
+    ///     Updating Enemies
+    ///     Spawning Bullets
+    ///     Updating Bullets
+    ///     Resolving Collisions (Attacks)
+    ///     Checking if the game has been won.
+    ///     Controlling UI State.
     /// </summary>
     public class GameController : MonoBehaviour
     {
+        // Tracks whether or not we're in play mode.
         public bool isGamePlaying = true;
+        
+        [Header("Camera")]
         
         public Transform camera;
         public Vector3 cameraOffset;
@@ -34,7 +48,6 @@ namespace MonoScript
         public float bulletSpeed = 10.0f;
         public float bulletLifeTime = 2.0f;
         public int bulletDamage = 1;
-        
         private List<Bullet> _bullets = new List<Bullet>();
 
         [Header("Enemy Spawner")] 
@@ -47,11 +60,13 @@ namespace MonoScript
         public int maxCurrentEnemies = 50;
 
         public float enemySpawnRate = 1f;
-        public float minEnemySpawnRate;
         
+        // These fields allow us to accelerate the spawn rate of enemies, increasing the difficulty.
+        public float minEnemySpawnRate = 0.1f;
         private float _enemySpawnRateIncrement;
         private float _timeSinceLastEnemySpawn = 0f;
 
+        // How far away the enemies can spawn from the player.
         public Vector2 minMaxSpawnDistance = new Vector2(5f, 10f);
 
         private List<Enemy> _enemies = new List<Enemy>();
@@ -65,21 +80,23 @@ namespace MonoScript
 
         void Start()
         {
-            // initialise player
+            // Initialise player.
             playerCurrentHealth = playerMaxHealth;
 
-            // initialise camera
+            // Initialise camera.
             cameraOffset = camera.transform.position;
             
-            // calculate our enemy spawn rate increment
+            // Calculate our enemy spawn rate increment.
             _enemySpawnRateIncrement = (enemySpawnRate - minEnemySpawnRate) / enemiesToSpawn;
         }
 
         private void Update()
         {
+            // We stop updating all our systems when the game is over. We rely on button events to restart the game.
             if (isGamePlaying)
             {
                 PlayerUpdate();
+                CameraUpdate();
                 EnemySpawnerUpdate();
                 EnemyUpdate();
                 GunUpdate();
@@ -90,7 +107,7 @@ namespace MonoScript
         }
         private void PlayerUpdate()
         {
-            // Move the player.
+            // Read keyboard input and map it to a direction
             var moveDirection = Vector3.zero;
             if (Input.GetKey(KeyCode.D))
                 moveDirection.x += 1;
@@ -104,44 +121,54 @@ namespace MonoScript
             if (Input.GetKey(KeyCode.S))
                 moveDirection.z -= 1;
 
-            player.transform.Translate(moveDirection * (PlayerMoveSpeed * Time.deltaTime));
+            // Ensure all directions are normalized!
+            player.transform.Translate(moveDirection.normalized * (PlayerMoveSpeed * Time.deltaTime));
+        }
 
+        private void CameraUpdate()
+        {
             // lock the camera to the player
             camera.transform.position = player.transform.position + cameraOffset;
-
-            
         }
+        
         private void EnemySpawnerUpdate()
         {
+            // This is our enemy spawn cooldown.
             _timeSinceLastEnemySpawn += Time.deltaTime;
+            
+            // Check if it's time to spawn an enemy.
             if (_timeSinceLastEnemySpawn >= enemySpawnRate && enemiesToSpawn > 0)
             {
                 if (currentEnemies < maxCurrentEnemies)
                 {
-                    // create a random spawn position within the spawn area that is 1 away from the player position
+                    // Generate a random position at a chosen radius from the player.
                     Vector2 randomDirection = Random.insideUnitCircle.normalized;
                     Vector3 randomWorldDirection = new Vector3(randomDirection.x, 0f, randomDirection.y);
                     var randomDistance = Random.Range(minMaxSpawnDistance.x, minMaxSpawnDistance.y);
                     Vector3 spawnPosition = randomDistance * randomWorldDirection + player.transform.position;
 
-                    // create and initialise the enemy.
+                    // Create and initialise the enemy.
                     Enemy enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
                     enemy.gameController = this;
                     enemy.currentHealth = enemy.maxHealth;
                     _enemies.Add(enemy);
-                    // update enemy data.
+                    
+                    // Update the enemy spawner data.
                     currentEnemies++;
                     enemiesToSpawn--;
 
-                    // increase the spawn rate of the next enemy.
+                    // Increase the spawn rate of the next enemy.
                     enemySpawnRate -= _enemySpawnRateIncrement;
                 }
 
+                // Reset the spawn cooldown.
                 _timeSinceLastEnemySpawn = 0f;
             }
         }
+        
         private void EnemyUpdate()
         {
+            // Move each enemy closer to the player.
             foreach (var enemy in _enemies)
             {
                 var enemyTransform = enemy.transform;
@@ -150,32 +177,41 @@ namespace MonoScript
             }
         }
 
+        /// <summary>
+        /// Controls the firing of bullets.
+        /// </summary>
         private void GunUpdate()
         {
+            // This is our gun firing cooldown.
             _timeSinceLastBullet += Time.deltaTime;
-            // get closest enemy
-            Enemy closestEnemy = null;
-            float closestEnemyDistance = float.PositiveInfinity;
-            foreach (var enemy in _enemies)
-            {
-                var distanceToEnemy = Vector3.Distance(player.transform.position, enemy.transform.position);
-                if (distanceToEnemy < closestEnemyDistance)
-                {
-                    closestEnemy = enemy;
-                    closestEnemyDistance = distanceToEnemy;
-                }
-            }
             
-            // Check if we've come off cooldown.
+            // Check if it's time to spawn a bullet.
             if (_timeSinceLastBullet > bulletCooldown)
             {
+                
+                // In order to fire a bullet, we first need to find the closest enemy.
+                Enemy closestEnemy = null;
+                float closestEnemyDistance = float.PositiveInfinity;
+                foreach (var enemy in _enemies)
+                {
+                    var distanceToEnemy = Vector3.Distance(player.transform.position, enemy.transform.position);
+                    if (distanceToEnemy < closestEnemyDistance)
+                    {
+                        closestEnemy = enemy;
+                        closestEnemyDistance = distanceToEnemy;
+                    }
+                }
+                
+                // Ensure there is an enemy on the map.
                 if (closestEnemy != null)
                 {
                     var enemyDistance = Vector3.Distance(player.transform.position, 
                         closestEnemy.transform.position);
                     
+                    // If there is an enemy, ensure that it's within the guns range.
                     if (enemyDistance < gunRange)
                     {
+                        // Create and Initialize a bullet, and reset the gun cooldown.
                         var playerPosition = player.transform.position;
                         var bullet = Instantiate(bulletPrefab, playerPosition, Quaternion.identity);
                         bullet.direction = (closestEnemy.transform.position - playerPosition).normalized;
@@ -191,7 +227,7 @@ namespace MonoScript
             var speed = bulletSpeed * Time.deltaTime;
             for (int i = 0; i < _bullets.Count; i++)
             {
-                // check the lifetime
+                // Check the bullet lifetime. This prevents the bullets living forever.
                 var bullet = _bullets[i];
                 if (bullet.timeAlive > bulletLifeTime)
                 {
@@ -210,16 +246,20 @@ namespace MonoScript
             HealthText.text = $"Health: {playerCurrentHealth}/{playerMaxHealth}";
             CurrentEnemiesText.text = $"Enemies Left {enemiesToSpawn + currentEnemies}";
         }
+        
+        /// <summary>
+        /// This will be called when a GameObject with a Bullet component enters a trigger.
+        /// </summary>
         public void BulletHit(Bullet bullet, Collider other)
         {
-            // check if the bullet hit an enemy
+            // Check if the bullet hit an enemy,.
             if (other.CompareTag("Enemy"))
             {
                 var enemy = other.GetComponent<Enemy>();
 
                 // Remove bullet damage from the enemies health.
-                // If it take lethal damage, destroy the enemy.
                 enemy.currentHealth -= bulletDamage;
+                // If the enemy has run out of health, kill it and update the Enemy Spawner.
                 if (enemy.currentHealth <= 0)
                 {
                     currentEnemies--;
@@ -227,11 +267,15 @@ namespace MonoScript
                     Destroy(enemy.gameObject);
                 }
                 
-                // destroy the bullet
+                // Destroy the bullet.
                 _bullets.Remove(bullet);
                 Destroy(bullet.gameObject);
             }
         }
+        
+        /// <summary>
+        /// This will be called when a GameObject with an Enemy Component on it enters a trigger.
+        /// </summary>
         public void EnemyHit(Enemy enemy, Collider other)
         {
             // If the enemy hit a player, reduce the players health.
@@ -248,6 +292,7 @@ namespace MonoScript
         }
         private void CheckWinCondition()
         {
+            // If the enemy spawner has run out of enemies, and all enemies on the field are dead, the game is won.
             if(enemiesToSpawn <= 0 && currentEnemies <= 0)
             {
                 WinGame();
@@ -268,6 +313,9 @@ namespace MonoScript
             isGamePlaying = false;
         }
 
+        /// <summary>
+        /// Reset the game by reloading the scene. 
+        /// </summary>
         public void StartGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);

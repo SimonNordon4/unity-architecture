ebc8b83412f0a25a157553b2af596b9a1fcea34a
Simon Nordon <86404984+SimonNordon4@users.noreply.github.com>
2023-01-13 13:09:36 +1000

    Added Classic

---
+++ Assets/Architecture/Classic/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Architecture.Classic
+{
+    public class Enemy : MonoBehaviour
+    {
+        public EnemySpawner spawner;
+        public Transform playerTransform;
+        public int damage = 1;
+
+        public bool dieOnTouch = false;
+        
+        private Health _health;
+        private Move _move;
+
+        private void Start()
+        {
+            _health = GetComponent<Health>();
+            _move = GetComponent<Move>();
+        }
+
+        public void Update()
+        {
+            var directionToPlayer = (playerTransform.position - transform.position).normalized;
+            _move.direction = directionToPlayer;
+
+            if (_health.currentHealth <= 0)
+            {
+                spawner.currentEnemies--;
+            }
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            // if the player is entered, deal damage
+            if (other.CompareTag("Player"))
+            {
+                other.GetComponent<Health>().currentHealth -= damage;
+
+                if (dieOnTouch)
+                {
+                    spawner.currentEnemies--;
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Architecture/Classic/Health.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Architecture.Classic
+{
+    public class Health : MonoBehaviour
+    {
+        public int maxHealth = 10;
+        public int currentHealth = 0;
+
+        private void Start()
+        {
+            currentHealth = maxHealth;
+        }
+
+        private void Update()
+        {
+            if(currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}

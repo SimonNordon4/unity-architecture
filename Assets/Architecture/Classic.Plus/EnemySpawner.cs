e47373e792b8005afd06d6baf896a6003c5f8ee0
Simon Nordon <86404984+SimonNordon4@users.noreply.github.com>
2023-01-13 13:10:47 +1000

    added classic plus.

---
+++ Assets/Architecture/Classic.Plus/EnemySpawner.cs
@@ -0,0 +1,83 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Architecture.Classic.Plus
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        // Enemy types we want to spawn.
+        public GameObject enemyPrefab;
+        public GameObject smallEnemyPrefab;
+        public GameObject bigEnemyPrefab;
+
+        // Probabilities of each enemy type spawning.
+        public float smallEnemySpawnChance = 0.1f;
+        public float bigEnemySpawnChance = 0.03f;
+
+        // Maximum enemies to spawn in level.
+        public int enemiesToSpawn = 100;
+
+        public int currentEnemies = 0;
+        public int maxCurrentEnemies = 50;
+
+        public float enemySpawnRate = 1f;
+        // Every time an enemy spawns, the next enemy will spawn slightly faster.
+        public float maxEnemySpawnRate = 0.1f;
+        private float _enemySpawnRateIncrement = 0.0f;
+        private float _timeSinceLastEnemySpawn = 0f;
+
+        private Transform _playerTransform;
+        public Vector2 minMaxSpawnDistance = new Vector2(5f, 10f);
+
+        private void Start()
+        {
+            // Initialise by calculating the decrement in spawn rate per enemy spawn.
+            _enemySpawnRateIncrement = (enemySpawnRate - maxEnemySpawnRate) / enemiesToSpawn;
+            _playerTransform = GameCatalog.Instance.PlayerInterface.transform;
+        }
+
+        void Update()
+        {
+            // If the enemy spawner cooldown has been satisfied, spawn an enemy.
+            _timeSinceLastEnemySpawn += Time.deltaTime;
+            if (_timeSinceLastEnemySpawn >= enemySpawnRate && enemiesToSpawn > 0)
+            {
+                if (currentEnemies < maxCurrentEnemies)
+                {
+                    // Create a random spawn position within the spawn area that is 1 away from the player position.
+                    Vector2 randomDirection = Random.insideUnitCircle.normalized;
+                    Vector3 randomWorldDirection = new Vector3(randomDirection.x, 0f, randomDirection.y);
+                    var randomDistance = Random.Range(minMaxSpawnDistance.x, minMaxSpawnDistance.y);
+                    Vector3 spawnPosition = randomDistance * randomWorldDirection + _playerTransform.position;
+
+                    var enemyToSpawn = enemyPrefab;
+                
+                    // roll to see if we spawn a small enemy.
+                    var spawnSmallEnemy = Random.Range(0,100) < smallEnemySpawnChance * 100f;
+                    if (spawnSmallEnemy)
+                    {
+                        enemyToSpawn = smallEnemyPrefab;
+                    }
+
+                    // roll to see if we spawn a big enemy.
+                    var spawnBigEnemy = Random.Range(0,100) < bigEnemySpawnChance *100f;
+                    if (spawnBigEnemy)
+                        enemyToSpawn = bigEnemyPrefab;
+                
+                    var newEnemy = Instantiate(enemyToSpawn, spawnPosition, Quaternion.identity);
+                    var newEnemyController = newEnemy.GetComponent<Enemy>();
+                    newEnemyController.spawner = this;
+                    newEnemyController.playerTransform = _playerTransform;
+                
+                    currentEnemies++;
+                    enemiesToSpawn--;
+                
+                    // Increase the spawn rate of the next enemy.
+                    enemySpawnRate -= _enemySpawnRateIncrement;
+                }
+                _timeSinceLastEnemySpawn = 0f;
+            }
+        }
+    }
+}
---
+++ Assets/Architecture/Classic.Plus/HealthBarUI.cs
@@ -0,0 +1,29 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Architecture.Classic.Plus
+{
+    /// <summary>
+    /// A UI element that displays a parents Health Component Data.
+    /// </summary>
+    public class HealthBarUI : MonoBehaviour
+    {
+        private Health _health;
+
+        public RectTransform healthBar;
+        public TextMeshProUGUI healthText;
+       
+        private void Start()
+        {
+            _health = GetComponentInParent<Health>();
+        }
+
+        private void Update()
+        {
+            healthBar.localScale = new Vector3((float)_health.Current / (float)_health.Max, 1, 1);
+            healthText.text =  _health.Current.ToString() + " / " + _health.Max.ToString();
+        }
+    }
+}
+
